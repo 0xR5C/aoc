2a508ffe19066caee84d717933f80aa9dfeff82e
0xr5c <foths72@gmail.com>
2025-12-06 19:22:33 +0200

    Day04 Part2

--- AdventOfCode-2025/Day04/day04.go
+++ AdventOfCode-2025/Day04/day04.go
@@ -40,50 +40,11 @@ func part1(input []string) {
 	count := 0
 	for i, line := range rollMap {
 		for j, val := range line {
-			rollsAround := 0
 			if val == "." {
 				continue
 			}
-			// check all directions
-			// i-1, j-1
-			if i-1 >= 0 && j-1 >= 0 && rollMap[i-1][j-1] == "@" {
-				rollsAround += 1
-			}
 
-			// i-1, j
-			if i-1 >= 0 && rollMap[i-1][j] == "@" {
-				rollsAround += 1
-			}
-
-			// i-1, j+1
-			if i-1 >= 0 && j+1 < len(line) && rollMap[i-1][j+1] == "@" {
-				rollsAround += 1
-			}
-
-			// i, j-1
-			if j-1 >= 0 && rollMap[i][j-1] == "@" {
-				rollsAround += 1
-			}
-
-			// i, j+1
-			if j+1 < len(line) && rollMap[i][j+1] == "@" {
-				rollsAround += 1
-			}
-
-			// i+1, j-1
-			if i+1 < len(rollMap) && j-1 >= 0 && rollMap[i+1][j-1] == "@" {
-				rollsAround += 1
-			}
-
-			// i+1, j
-			if i+1 < len(rollMap) && rollMap[i+1][j] == "@" {
-				rollsAround += 1
-			}
-
-			// i+1, j+1
-			if i+1 < len(rollMap) && j+1 < len(line) && rollMap[i+1][j+1] == "@" {
-				rollsAround += 1
-			}
+			rollsAround := checkAround(i, j, len(rollMap), len(line), rollMap)
 
 			if rollsAround < 4 {
 				count += 1
@@ -97,7 +58,34 @@ func part1(input []string) {
 
 func part2(input []string) {
 	fmt.Print("Part2 ")
-	fmt.Println()
+	rollMap := createRollMap(input)
+	totalCount := 0
+	count := 1
+	for count > 0 {
+		cpy := make([][]string, len(rollMap))
+		count = 0
+		for i, line := range rollMap {
+			// create copy of line
+			cpy[i] = make([]string, len(line))
+			copy(cpy[i], rollMap[i])
+
+			for j, val := range line {
+				if val == "." {
+					continue
+				}
+
+				rollsAround := checkAround(i, j, len(rollMap), len(line), rollMap)
+
+				if rollsAround < 4 {
+					count += 1
+					cpy[i][j] = "."
+				}
+			}
+		}
+		rollMap = cpy
+		totalCount += count
+	}
+	fmt.Println("Total number of accessible rolls is", totalCount)
 }
 
 // Helper functions
@@ -112,4 +100,50 @@ func createRollMap(input []string) [][]string {
 		rollMap = append(rollMap, temp)
 	}
 	return rollMap
+}
+
+func checkAround(i, j, lenI, lenJ int, rollMap [][]string) int {
+	rollsAround := 0
+	// check all directions
+	// i-1, j-1
+	if i-1 >= 0 && j-1 >= 0 && rollMap[i-1][j-1] == "@" {
+		rollsAround += 1
+	}
+
+	// i-1, j
+	if i-1 >= 0 && rollMap[i-1][j] == "@" {
+		rollsAround += 1
+	}
+
+	// i-1, j+1
+	if i-1 >= 0 && j+1 < lenJ && rollMap[i-1][j+1] == "@" {
+		rollsAround += 1
+	}
+
+	// i, j-1
+	if j-1 >= 0 && rollMap[i][j-1] == "@" {
+		rollsAround += 1
+	}
+
+	// i, j+1
+	if j+1 < lenJ && rollMap[i][j+1] == "@" {
+		rollsAround += 1
+	}
+
+	// i+1, j-1
+	if i+1 < lenI && j-1 >= 0 && rollMap[i+1][j-1] == "@" {
+		rollsAround += 1
+	}
+
+	// i+1, j
+	if i+1 < lenI && rollMap[i+1][j] == "@" {
+		rollsAround += 1
+	}
+
+	// i+1, j+1
+	if i+1 < lenI && j+1 < lenJ && rollMap[i+1][j+1] == "@" {
+		rollsAround += 1
+	}
+
+	return rollsAround
 }
